52e60a3931f887cb3bbf734e1fe200fd7c429274
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-14 11:32:40 -0600

    Added a few basic types.

---
+++ Source/ConsoleApplication1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Learning;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var decisionVariable = new DecisionVariable<string, int>(s => s.Length, 0, 50);
+        }
+    }
+}
---
+++ Source/Learning.Net461/DecisionVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Learning
+{
+    public class DecisionVariable<TSample, TValue>
+    {
+        public DecisionVariable(Expression<Func<TSample, TValue>> property, params TValue[] rangeValues)
+        {
+            Property = property;
+            Range = new List<TValue>(rangeValues);
+        }
+
+        public Expression<Func<TSample, TValue>> Property { get; }
+
+        public IList<TValue> Range { get; }
+    }
+}
